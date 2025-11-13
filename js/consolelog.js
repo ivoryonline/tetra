ba3aae45133bd8d3ebff393e4d08198f925a89c9
ivoryonline <twixedcoord@gmail.com>
2025-11-13 17:49:25 +0000

    add cute prints to console (shoutout to https://github.com/ariezmeoww/vortexi-src — I took some of your code for it)

---
+++ js/consolelog.js
@@ -0,0 +1,13 @@
+console.log(
+    `%c>w<`,
+    `color: #5BCEFA; font-size: 80px; font-weight: bold; text-shadow: 2px 2px 0 #F5A9B8, 4px 4px 0 #ffffff;`
+);
+
+console.log(
+    `%cIf this was a company website and I was the CEO, I'd put a redirect to getting a job for the company here :-3\n(sadly I'm not a CEO of any company right now though...)`,
+    "color: #ff004cff; font-size: 18px; font-weight: 500;"
+);
+
+console.log(
+    `boo! good luck with whatever console thing you're trying to do!`
+);
